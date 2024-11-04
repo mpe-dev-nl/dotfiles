03f2a4a1e83a81d8574893acd04dd49360e91b4d
Marcel Perdok <mpe.dev.nl@gmail.com>
2024-11-04 14:54:41 +0100

    Next

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
 }
